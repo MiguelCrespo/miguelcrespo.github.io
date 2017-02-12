53ef5e931b1d716a6d3c49064d524594ff142c5f
Miguel Crespo <miguel.crespo6@gmail.com>
2017-02-11 23:25:05 -0500

    Setup initial components

--- app/index.js
+++ app/index.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
+// Our application imports
+import App from './components/App';
+import Home from './components/Home';
+
 ReactDOM.render((
-  <Router history={history}>
+  <Router history={hashHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={LandingCities} />
-      <Route path="/:city" component={Home} />
-      <Route path="/:city/:establishmentUrl" component={EstablishmentView} />
+      <IndexRoute component={Home} />
     </Route>
   </Router>
 ), document.getElementById('container'));
