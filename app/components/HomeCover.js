ab632b33f8d6603461f0b52d362edc77490f9178
Miguel Crespo <miguel.crespo6@gmail.com>
2017-02-15 00:58:17 -0500

    Created a component that simulate the typing of a person

--- app/components/HomeCover.js
+++ app/components/HomeCover.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
 
+import Typer from './typer/Typer';
 export default class HomeCover extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            words: ['Frontend Developer', 'Backend Developer', 'Full Stack Developer']
+        };
+    }
+
     render() {
         return <div className="home-cover__content">
 
-            <h1 className="home-cover__content__header">
-                Frontend Developer
-            </h1>
+            <Typer words={this.state.words}
+                   classes="home-cover__content__header" speed="100"/>
 
             <span className="home-cover__content__header__content">
                 Hello, I'm Miguel a software developer in love with technology
