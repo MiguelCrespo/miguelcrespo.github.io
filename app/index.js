fc3c00ae36df3f11e9a8ab0b6549559d36637575
Miguel Crespo <miguel.crespo6@gmail.com>
2017-02-16 18:19:04 -0500

    Added webfonts loader to load fonts async

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import WebFont from 'webfontloader';
 
 // Our application imports
 import App from './components/App';
@@ -8,6 +9,13 @@ import Home from './components/Home';
 
 // Styles
 import './index.scss';
+
+WebFont.load({
+    google: {
+        families: ['Roboto:300,400,700']
+    },
+    timeout: 2000
+});
 
 ReactDOM.render((
   <Router history={browserHistory}>
